733c8351ec8bf726fafa7553d4f85eea9a89121d
unknown <supernftier@gmail.com>
2022-04-10 09:51:48 +0300

    wallet connect

--- src/ContentComponent.js
+++ src/ContentComponent.js
@@ -7,9 +7,12 @@ import React, { useEffect, useState } from "react"
 const ContentComponent = () => {
   const { activate, deactivate, library, account } = useWeb3React()
   const [quantity, setQuantity] = useState(1)
+  const [connectedFlag, setConnectedWalletFlag] = useState(null)
+  const [mintPrice, setMintPrice] = useState(0.0824)
+  const [isMintOpen, SetMintOpenFlag] = useState(false)
   const [TotalSupply, setTotalSupply] = useState(10000)
   const injected = new InjectedConnector({
-    supportedChainIds: [1, 5],
+    supportedChainIds: [1, 4],
   })
   const increase = () => {
     let number = quantity + 1
@@ -20,16 +23,12 @@ const ContentComponent = () => {
     if (quantity > 1) setQuantity(number)
   }
   const mintNFT = async () => {
-    try {
-      await activate(injected)
-    } catch (ex) {
-      console.log(ex)
-    }
+
 
     const signer = (new ethers.providers.Web3Provider(window.ethereum)).getSigner();
     const contract = new ethers.Contract(CONTRACT.address, CONTRACT.abi, signer);
 
-    const price = ethers.utils.parseUnits((quantity * .0824).toString(), 'ether')
+    const price = ethers.utils.parseUnits((quantity * mintPrice).toString(), 'ether')
 
     try {
       const ans = await contract.mint(quantity, {
@@ -44,33 +43,60 @@ const ContentComponent = () => {
     return false;
   };
 
+  const connectwallet = async () => {
+    try {
+      await activate(injected)
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
   useEffect(() => {
   (async () => {
     const signer = (new ethers.providers.Web3Provider(window.ethereum)).getSigner();
     const contract = new ethers.Contract(CONTRACT.address, CONTRACT.abi, signer);
-    // get the 
     const supply = await contract.totalSupply();
+    const tokenPrice = await contract.getMintPrice();
+    const mintOpen = await contract.isMintOpen();
 
     setTotalSupply(supply.toNumber());
+    SetMintOpenFlag(mintOpen);
+    setMintPrice(parseFloat(tokenPrice / Math.pow(10, 18)));
   })()
   }, [])
+  useEffect(() => {
+    console.log("aa",account)
+    setConnectedWalletFlag(account == undefined ? null : account)
+  })
 
   return (
     <React.Fragment>
       <div id="mintsection">
-        <div id="countdiv">
-          {TotalSupply}/10000 Minted
+        {
+          isMintOpen ? <div id="closedtitle">mint is currently closed</div>
+                     : 
+                     <div>
+                        { 
+                          connectedFlag == null ? <button id="connectwalletbtn" onClick={connectwallet}>connect</button>
+                                         :
+                          <div>
+                            <div id="countdiv">
+                              {TotalSupply}/10000 Minted
+                            </div>
+                            <div id="countdiv">
+                              Mint your hoops now.
+                            </div>
+                            <div id="countdiv">
+                              <div id="selectcount" onClick={decrease}>-</div>
+                              <div id="tokencount">{quantity}</div>
+                              <div id="selectcount" onClick={increase}>+</div>
+                            </div>
+                            <button id="mintbtn" onClick={mintNFT}>Mint</button>
+                          </div>
+                        }
+                    </div>
+        }
         </div>
-        <div id="countdiv">
-          Mint your hoops now.
-        </div>
-        <div id="countdiv">
-          <div id="selectcount" onClick={decrease}>-</div>
-          <div id="tokencount">{quantity}</div>
-          <div id="selectcount" onClick={increase}>+</div>
-        </div>
-        <button id="mintbtn" onClick={mintNFT}>Mint</button>
-      </div>
+        
     </React.Fragment>
   )
 }
