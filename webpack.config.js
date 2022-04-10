140c74f8ce078370f34e35481d108cfd7d60d54f
shawticus <shawmakesmagic@gmail.com>
2022-04-09 19:15:02 -0700

    Split bundles so we only need to push main

--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,30 @@ module.exports = {
  output: {
    path: __dirname + '/dist',
    publicPath: '/',
-   filename: 'bundle.js'
  },
+optimization: {splitChunks: {
+  chunks: 'all',
+  minSize: 20000,
+  minRemainingSize: 0,
+  minChunks: 1,
+  maxAsyncRequests: 30,
+  maxInitialRequests: 30,
+  enforceSizeThreshold: 50000,
+  cacheGroups: {
+    defaultVendors: {
+      test: /[\\/]node_modules[\\/]/,
+      priority: -10,
+      reuseExistingChunk: true,
+      idHint: 'vendors',
+      name: "vendors"
+    },
+    default: {
+      minChunks: 2,
+      priority: -20,
+      reuseExistingChunk: true,
+    },
+  },
+} },
  devServer: {
    static: {
      directory: path.join(__dirname, 'dist')
